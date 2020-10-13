cb02c952a097192e2bb166209458a061e84398e5
RubenEscalante <chabo_2006@hotmail.com>
2020-10-13 00:09:00 -0600

    Carrito y menu funcionando al 100%

--- appDelivery/src/app/menu/services/menucart.service.ts
+++ appDelivery/src/app/menu/services/menucart.service.ts
@@ -12,19 +12,19 @@ export class MenucartService {
   }
 
   // tslint:disable-next-line:typedef
-  //Agrega al localstorage los productos
+  // Agrega al localstorage los productos
   addToCar(items) {
-    if (localStorage.getItem('cart') === null) {
+    if (localStorage.getItem('cart') === 'null' || localStorage.getItem('cart') === null) {
       this.cart = [];
       this.cart.push(items);
       localStorage.setItem('cart', JSON.stringify(this.cart));
     } else {
       this.actualCart = this.getCartItems();
-      let person = this.actualCart.find((person) => {
+      const person = this.actualCart.find((person) => {
         return person.$key === items.$key && person.dough === items.dough;
       });
-      if (person && person['quantity']) {
-        person['quantity'] = person['quantity'] + items.quantity;
+      if (person && person.quantity) {
+        person.quantity = person.quantity + items.quantity;
         localStorage.setItem('cart', JSON.stringify(this.actualCart));
       }
       if (person === undefined) {
@@ -39,17 +39,23 @@ export class MenucartService {
     }
   }
 
-  //Devuelve lo que esta actualmente en el localstorage
+  // Devuelve lo que esta actualmente en el localstorage
   getCartItems() {
     return JSON.parse(localStorage.getItem('cart'));
   }
 
   getCartLength() {
-    this.actualCart = this.getCartItems();
-    if (localStorage.getItem('cart') === null) {
+    if (localStorage.getItem('cart') === null || localStorage.getItem('cart') === 'null') {
       return null;
     }
-    return this.actualCart.length;
+    if (localStorage.getItem('cart') !== 'null') {
+      this.actualCart = this.getCartItems();
+    }
+    let total = 0;
+    for (let num of this.actualCart) {
+      total += num.quantity;
+    }
+    return total;
   }
 
 }
