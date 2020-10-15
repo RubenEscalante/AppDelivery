bda0d1138bf61ab530c8ea1411a92007a0a92e32
RicardoM99160 <inuriqui_cool2@outlook.com>
2020-10-14 19:04:22 -0600

    llenando datos en la confirmación del pedido

--- appDelivery/src/app/datos-pedido/components/datos/datos.component.ts
+++ appDelivery/src/app/datos-pedido/components/datos/datos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../models/usuario';
 
 @Component({
   selector: 'app-datos',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatosComponent implements OnInit {
 
+  //Este simula ser el usuario que estará almacenado en el local storage
+  usuarioRegistrado:Usuario = {
+    nombre:"José Ricardo Majano De Paz",
+    correo:"ricardo.majano@pupusa.com",
+    telefono:"2255-55555",
+    direcciones:[
+      {nombre:"Dirección 1", direccion:"Colonia el pepeto, pasaje 11, casa #54", municipio:"Mejicanos", departamento:"San Salvador"},
+      {nombre:"Dirección 2", direccion:"Colonia el pepeto, pasaje 11, casa #54", municipio:"Mejicanos", departamento:"San Salvador"},
+      {nombre:"Dirección 3", direccion:"Colonia el pepeto, pasaje 11, casa #54", municipio:"Mejicanos", departamento:"San Salvador"}
+    ]
+  };
+
+  //Este es el usuario que utilizo para mostrar los datos
+  public usuario:Usuario;
   constructor() { }
 
   ngOnInit(): void {
+
+    //Aquí debería de obtener los datos del usuario que inició sesión, ya sea del local storage o de la bd
+    this.usuario = this.usuarioRegistrado;
   }
+
+
 
 }
