07551644e00dbab95afdc084d3b86433e52c77d1
RubenEscalante <chabo_2006@hotmail.com>
2020-10-17 15:42:29 -0600

    Login funcionando, falta mejorar diseño y formularios

--- appDelivery/src/app/layout/components/main-layout/main-layout.component.ts
+++ appDelivery/src/app/layout/components/main-layout/main-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MenucartService} from '../../../menu/services/menucart.service';
+import {AuthService} from '../../../usuarioauth/services/auth.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -8,10 +9,12 @@ import {MenucartService} from '../../../menu/services/menucart.service';
 })
 export class MainLayoutComponent implements OnInit {
   ///totalCartItem: number;
-  constructor(public menuCartservice: MenucartService) { console.log('Main layout constructor called'); }
+
+  constructor(public menuCartservice: MenucartService, public authService: AuthService) { console.log('Main layout constructor called'); }
 
   ngOnInit(): void {
    // this.totalCartItem = this.menuCartservice.getCartLength(); //Para despues
+
   }
 
 }
