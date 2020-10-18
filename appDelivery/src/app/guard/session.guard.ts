ec74b295e42d69f58d02cbdce0a41d36cc10ef74
shyfrost <alxfr0s7@gmail.com>
2020-10-18 00:42:29 -0600

    Guard Funcionando para appDelivery

---
+++ appDelivery/src/app/guard/session.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from '../usuarioauth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionGuard implements CanActivateChild {
+  constructor(
+    public authService: AuthService,
+    public router: Router
+
+  ){}
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.authService.isLoggedIn === true){
+        this.router.navigate(['menu'])
+        return false;
+      
+      }
+      else {
+        return true;
+      }
+  }
+  
+}
--- appDelivery/src/app/layout/layout-routing.module.ts
+++ appDelivery/src/app/layout/layout-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { SecondaryLayoutComponent} from './components/secondary-layout/secondary-layout.component';
-
+import { AuthGuard} from '../guard/auth.guard';
+import { SessionGuard} from '../guard/session.guard';
 
 // Enrutamiento para el layout principal, compartido por el carrito de compras, el menu...
 const routes: Routes = [
@@ -19,11 +20,14 @@ const routes: Routes = [
       { path: 'menu',
         loadChildren: ()=> import('../menu/menu.module').then(mod => mod.MenuModule)},
       { path: 'carrito',
-        loadChildren: ()=> import('../carrito/carrito.module').then(mod => mod.CarritoModule)},
+        loadChildren: ()=> import('../carrito/carrito.module').then(mod => mod.CarritoModule),
+        canActivateChild: [AuthGuard]},
       { path: 'datos-pedido',
-        loadChildren: ()=> import('../datos-pedido/datos-pedido.module').then(mod => mod.DatosPedidoModule)},
+        loadChildren: ()=> import('../datos-pedido/datos-pedido.module').then(mod => mod.DatosPedidoModule),
+        canActivateChild: [AuthGuard]},
       { path: 'perfil-usuario',
-        loadChildren: ()=> import('../panel-usuario/panel-usuario.module').then(mod => mod.PanelUsuarioModule)}
+        loadChildren: ()=> import('../panel-usuario/panel-usuario.module').then(mod => mod.PanelUsuarioModule),
+        canActivateChild: [AuthGuard]}
     ]
   },
   // Enrutamiento para un layout secundario, de ser necesario
@@ -32,11 +36,14 @@ const routes: Routes = [
     component: SecondaryLayoutComponent,
     children:  [
       { path: 'autentificacion',
-        loadChildren: ()=> import('../usuarioauth/sign-in/sign-in.module').then(mod => mod.SignInModule)},
+        loadChildren: ()=> import('../usuarioauth/sign-in/sign-in.module').then(mod => mod.SignInModule),
+        canActivateChild: [SessionGuard]},
       { path: 'verificar-email',
-        loadChildren: ()=> import('../usuarioauth/verificar-email/verificar-email.module').then(mod => mod.VerificarEmailModule)},
+        loadChildren: ()=> import('../usuarioauth/verificar-email/verificar-email.module').then(mod => mod.VerificarEmailModule),
+        canActivateChild: [AuthGuard]},
       { path: 'clave-perdida',
-        loadChildren: ()=> import('../usuarioauth/clave-perdida/clave-perdida.module').then(mod => mod.ClavePerdidaModule)}
+        loadChildren: ()=> import('../usuarioauth/clave-perdida/clave-perdida.module').then(mod => mod.ClavePerdidaModule),
+        canActivateChild: [SessionGuard]}
     ]
   }
 
